bbc74066dd9d45e7b71c850dcd0229991ac7d353
Ezechias22 <toussaintezechias16@gmail.com>
2025-09-30 22:13:13 -0300

    Add production build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Analyze_Graphics/', // <--- Ajoute sa pou GitHub Pages
   plugins: [react()],
   build: {
     target: 'esnext',
@@ -18,7 +19,6 @@ export default defineConfig({
     }
   },
   server: {
-    // https: true, // Removed or replace with a valid HTTPS config object if needed
     host: true
   },
   optimizeDeps: {
